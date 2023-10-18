d7c4299a0b156fa97a7a9395dd6235b79fb8e847
陈柄宏 <chenbh@teaming.com.cn>
2023-10-18 14:18:32 +0800

    feat: 模仿vue，实现数据双向绑定
    使用Proxy实现，并且使用虚拟DOM层级比较优化渲染

--- 模板引擎/v1/index.js
+++ 模板引擎/v1/index.js
@@ -26,6 +26,11 @@ function toClassList(classAttr) {
 function processAttrs(el) {
   const codeList = [];
   el.getAttributeNames().forEach((attrName) => {
+    if (attrName.startsWith("@")) {
+      const express = processAttr(el, attrName);
+      codeList.push(`'${attrName}': $event => {${express}}`);
+      return;
+    }
     if (attrName.startsWith(":") || attrName.startsWith("v-")) {
       const express = processAttr(el, attrName);
       codeList.push(`'${attrName}': ${express ? express : undefined}`);
@@ -127,13 +132,13 @@ function processHtml(el, pass) {
     return express;
   }
   let code = "";
-  code += `createElement('${el.tagName}',`;
+  code += `createVDom('${el.tagName}',`;
   code += processAttrs(el) + ",\n";
   code += [...el.childNodes]
     .map((child) => processDom(child))
     .filter((item) => item)
     .join(",\n");
-  code += `)//createElement\n`;
+  code += `)//createVDom\n`;
   if (pass) el.setAttribute("v-tmp-pass", pass);
   return code;
 }
@@ -156,7 +161,7 @@ function processDom(el) {
       break;
     case 3:
       if (!el.textContent.trim()) return;
-      code += `createElement('TEXT', null, \`${processText(el)}\`)`;
+      code += `createVDom('TEXT', null, \`${processText(el)}\`)`;
       break;
     default:
       break;
@@ -164,26 +169,81 @@ function processDom(el) {
   return code;
 }
 
-function createElement(name, attrs, ...children) {
-  // console.log(arguments, { name, children, attrs });
+function createVDom(name, attrs, ...children) {
   return { name, attrs, children };
 }
 
-function render(el, { data, methods = {} }) {
-  if (typeof el === "string") el = document.querySelector(el);
-  if (!(el instanceof HTMLElement)) return;
-  if (typeof data !== "object" || data === null) return;
+/**
+ * 比较两个对象的字面量是否一致，未考虑循环引用
+ * @param {any} obj1 比较对象1
+ * @param {any} obj2 比较对象2
+ * @param {boolean} strict 是否严格比较
+ * @returns 是否一致
+ */
+function equal(obj1, obj2, strict = false) {
+  const type = typeof obj1;
+  if (type !== typeof obj2) {
+    return false;
+  }
+  if (type === "object") {
+    if (obj1 === null) return obj2 === null;
+    if (obj2 === null) return obj1 === null;
+    if (Array.isArray(obj1) !== Array.isArray(obj2)) {
+      return false;
+    }
+    if (strict) {
+      const keys1 = Object.keys(obj1);
+      const keys2 = Object.keys(obj2);
+      // console.log({ keys1, keys2 });
+      if (keys1.length !== keys2.length) return false;
+      for (let key of keys1) {
+        if (!equal(obj1[key], obj2[key])) return false;
+      }
+      return true;
+    } else {
+      if (Array.isArray(obj1)) {
+        if (obj1.length !== obj2.length) return false;
+        // [empty] 和 [undefined]
+        // Object.keys()不能取到 empty
+        for (let i = 0; i < obj1.length; i++) {
+          if (!equal(obj1[i], obj2[i])) return false;
+        }
+        return true;
+      }
+      // {a: undefined} 和 {}
+      for (let key of new Set([...Object.keys(obj1), ...Object.keys(obj2)])) {
+        if (!equal(obj1[key], obj2[key])) return false;
+      }
+      return true;
+    }
+  }
+  return Object.is(obj1, obj2);
+}
 
-  const code = `[${[...el.childNodes]
-    .map(processDom)
-    .filter((item) => item)
-    .join(",\n")}]`;
+class Vue {
+  #getVDomList;
+  #lastVDomList = [];
 
-  el.innerHTML = "";
+  constructor(el, { data = {}, methods = {} }) {
+    if (typeof el === "string") el = document.querySelector(el);
+    if (!(el instanceof HTMLElement)) return;
+    if (typeof data !== "object" || data === null) return;
 
-  const topFunc = new Function(
-    ...Object.keys(data),
-    `
+    const code = `[${[...el.childNodes]
+      .map(processDom)
+      .filter((item) => item)
+      .join(",\n")}]`;
+
+    el.innerHTML = "";
+
+    // 数据双向绑定
+    const proxy = this.createProxyRecursively(data);
+
+    // 获取虚拟DOM的方法
+    this.#getVDomList = () =>
+      new Function(
+        ...Object.keys(proxy),
+        `
     ${Object.entries(methods)
       .map(
         ([name, fn]) => `function ${fn}
@@ -191,62 +251,152 @@ function render(el, { data, methods = {} }) {
       `
       )
       .join("\n")}
-    ${createElement}
+    ${createVDom}
     return ${code}
   `
-  );
+      )
+        .call(this, ...Object.values(proxy), el)
+        .filter((item) => item);
 
-  console.log(topFunc.toString());
-  const result = topFunc
-    .call(
-      { ...data, $data: data, $methods: methods },
-      ...Object.values(data),
-      el
-    )
-    .filter((item) => item);
-  console.log({ data, result });
+    // this上的数据双向绑定
+    Object.keys(proxy).forEach((key) => {
+      Object.defineProperty(this, key, {
+        get() {
+          return proxy[key];
+        },
+        set(value) {
+          proxy[key] = value;
+        },
+      });
+    });
 
-  function commitNode(nodeOption) {
-    const { name, attrs, children } = nodeOption;
+    this.$el = el;
+    this.$data = proxy;
+    this.$methods = methods;
+    this.#lastVDomList = [];
+
+    console.log(this);
+    this.render();
+  }
+
+  createProxyRecursively(obj) {
+    if (typeof obj !== "object" || obj === null) return obj;
+    if (obj instanceof Date) return obj;
+    Object.keys(obj).forEach((key) => {
+      obj[key] = this.createProxyRecursively(obj[key]);
+    });
+    const self = this;
+    return new Proxy(obj, {
+      set(target, property, value, receiver) {
+        target[property] = value;
+        console.log("this", this);
+        self.render();
+        return true;
+      },
+    });
+  }
+
+  #setAttrbute(node, attrName, attrValue) {
+    if (!(node instanceof HTMLElement)) return;
+    // TODO 注册的指令
+    if (attrName.startsWith("v-")) {
+      if ("v-else,v-else-if".split(",").includes(attrName)) {
+        console.error(`${attrName} should after v-if`);
+        return;
+      }
+      console.log({ attrName });
+      return;
+    }
+    // 事件
+    if (attrName.startsWith("@")) {
+      node.addEventListener(attrName.slice(1), attrValue);
+      return;
+    }
+    switch (attrName) {
+      case ":key":
+        // TODO :key
+        break;
+      case ":style":
+        processStyle(node, attrValue);
+        break;
+      case ":class":
+        processClass(node, attrValue);
+        break;
+      default:
+        let _attrName = attrName.startsWith(":") ? attrName.slice(1) : attrName;
+        node.setAttribute(_attrName, attrValue);
+        break;
+    }
+  }
+
+  #createNode(vDom) {
+    const { name, attrs, children } = vDom;
     let node;
     if (name === "TEXT") node = document.createTextNode(children[0]);
     else {
       node = document.createElement(name);
-      if ("v-show" in attrs && !attrs["v-show"]) {
-        attrs[":style"] = attrs[":style"] ?? {};
-        attrs["display"] = "none";
+      for (let attrName in attrs) {
+        this.#setAttrbute(node, attrName, attrs[attrName]);
       }
-      for (let attr in attrs) {
-        const value = attrs[attr];
-        switch (attr) {
-          case ":key":
-            // TODO :key
-            break;
-          case ":style":
-            processStyle(node, value);
-            break;
-          case ":class":
-            processClass(node, value);
-            break;
-          // TODO 注册的指令
-          default:
-            node.setAttribute(attr, value);
-            break;
-        }
-      }
-      commit(children.filter((item) => item)).forEach((child) =>
-        node.appendChild(child)
-      );
     }
+    console.log("createNode", node);
     return node;
   }
-  function commit(nodeOptionList) {
-    let nodes = [];
-    for (let i = 0; i < nodeOptionList.length; i++) {
-      let nodeOption = nodeOptionList[i];
-      nodes.push(commitNode(nodeOption));
+
+  diff(vDomList, lastVDomList, parent) {
+    if (parent.nodeType === 3) {
+      console.log("update", lastVDomList[0], "->", vDomList[0]);
+      parent.textContent = vDomList[0];
+      return;
     }
-    return nodes;
+    if (!(parent instanceof HTMLElement)) return;
+    const length = Math.max(vDomList.length, lastVDomList.length);
+    for (let i = 0; i < length; i++) {
+      const vDom = vDomList[i];
+      const lastVDom = lastVDomList[i];
+      const node = parent.childNodes[i];
+      if (vDom === undefined) {
+        console.log("remove", lastVDom.name);
+        node.remove();
+        continue;
+      }
+      if (lastVDom === undefined) {
+        console.log("create", vDom.name);
+        let newNode = this.#createNode(vDom);
+        this.diff(vDom.children, [], newNode);
+        parent.appendChild(newNode);
+        continue;
+      }
+      if (vDom.name !== lastVDom.name) {
+        console.log("update", lastVDom.name, "->", vDom.name);
+        let newNode = this.#createNode(vDom);
+        this.diff(vDom.children, [], node);
+        parent.insertBefore(newNode, node);
+        node.remove();
+        continue;
+      }
+      [
+        ...new Set(
+          ...Object.keys(vDom.attrs ?? {}),
+          ...Object.keys(lastVDom.attrs ?? {})
+        ),
+      ].forEach((attr) => {
+        if (!(attr in vDom.attrs)) {
+          node.removeAttribute(attr);
+          return;
+        }
+        if (equal(vDom.attrs[attr], lastVDom.attrs[attr])) return;
+        this.#setAttrbute(node, attr, vDom.attrs[attr]);
+      });
+      this.diff(vDom.children, lastVDom.children, node);
+    }
   }
-  commit(result).forEach((child) => el.appendChild(child));
+
+  render() {
+    const vDomList = this.#getVDomList();
+    console.log({ vDomList, lastVDomList: this.#lastVDomList });
+    // 重新渲染时候，不要全部更新，比较更新（尽量最小更新，否则会有一些用户体验问题，例如：input框失去焦点）
+    this.diff(vDomList, this.#lastVDomList, this.$el);
+    this.#lastVDomList = vDomList;
+  }
 }
